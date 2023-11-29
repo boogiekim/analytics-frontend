0e070af9f3cdb457b73964c49b89c7d7bd4502e7
John Kim <john.kim@ascendingdc.com>
2023-11-29 12:04:33 -0500

    added call-on-tab functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/layout/Header';
 import Navigation from './components/layout/Navigation';
 import TableauView from './components/views/TableauView';
-import QuicksightView from './components/views/QuicksightView';
+import QuicksightView from './components/views/QuicksightViewWithAPI';
 import Home from './components/views/Home';
 import Footer from './components/layout/Footer';
 import 'tabler-react/dist/Tabler.css';
--- src/components/views/QuicksightViewWithAPI.js
+++ src/components/views/QuicksightViewWithAPI.js
@@ -3,6 +3,27 @@ import MainLayout from '../layout/MainLayout';
 import quicksightIcon from '../../assets/images/quicksight-icon.png'
 import axios from 'axios';
 
+const globalTBParams = [
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_d4416e80-fbc1-4486-b263-17b36ff2005e,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_b257153f-c15c-40ce-bf24-cea54025675b'},
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_d4416e80-fbc1-4486-b263-17b36ff2005e,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_8d22f8d1-daf3-469e-9a70-9268749ab4b5'},
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_d4416e80-fbc1-4486-b263-17b36ff2005e,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_dd3b3178-6881-4e30-a134-e9a7e78bd720'}
+];
+
+const countryParams = [
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_15800c89-5088-4fda-9b6f-48cdea419550,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_0e95b90d-e6ef-459b-9bb4-d5db3cc7a956'},
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_15800c89-5088-4fda-9b6f-48cdea419550,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_1d592196-4e93-459e-8760-fabcf402a388'},
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_15800c89-5088-4fda-9b6f-48cdea419550,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_e356888f-51e6-4b8e-bf75-dac2bcacf283'}
+];
+
+const demographicsParams = [
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_a9bc8ba9-6318-4bf2-a39c-179057f694b5,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_8e5d5400-ff65-4b89-b7ae-d069af5306e2'},
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_a9bc8ba9-6318-4bf2-a39c-179057f694b5,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_90e631f5-dad6-43fa-bf9e-073a9133a276'}
+];
+
+const budgetExpenditureParams = [
+  { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_c7d97bbf-5061-4da0-bcd2-32fc5280dabd,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_da0670a6-3ca9-4695-b4c6-aea55b27c1e1'}
+        ];
+
 const QuicksightView = () => {
   const [activeTab, setActiveTab] = useState('globalTB');
   const [viewUrls, setViewUrls] = useState({
@@ -16,78 +37,41 @@ const QuicksightView = () => {
   useEffect(() => {
     const fetchUrls = async () => {
       try {
-        const urlsData = {...viewUrls};
-
-        const globalTBParams = [
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_d4416e80-fbc1-4486-b263-17b36ff2005e,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_b257153f-c15c-40ce-bf24-cea54025675b'},
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_d4416e80-fbc1-4486-b263-17b36ff2005e,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_8d22f8d1-daf3-469e-9a70-9268749ab4b5'},
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_d4416e80-fbc1-4486-b263-17b36ff2005e,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_dd3b3178-6881-4e30-a134-e9a7e78bd720'}
-        ];
-
-        const countryParams = [
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_15800c89-5088-4fda-9b6f-48cdea419550,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_0e95b90d-e6ef-459b-9bb4-d5db3cc7a956'},
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_15800c89-5088-4fda-9b6f-48cdea419550,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_1d592196-4e93-459e-8760-fabcf402a388'},
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_15800c89-5088-4fda-9b6f-48cdea419550,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_e356888f-51e6-4b8e-bf75-dac2bcacf283'}
-        ];
-
-        const demographicsParams = [
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_a9bc8ba9-6318-4bf2-a39c-179057f694b5,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_8e5d5400-ff65-4b89-b7ae-d069af5306e2'},
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_a9bc8ba9-6318-4bf2-a39c-179057f694b5,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_90e631f5-dad6-43fa-bf9e-073a9133a276'}
-        ];
-
-        const budgetExpenditureParams = [
-          { type: 'visual', id: '8a830b7e-72a1-4434-b0c8-9e0d68db3d75,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_c7d97bbf-5061-4da0-bcd2-32fc5280dabd,8a830b7e-72a1-4434-b0c8-9e0d68db3d75_da0670a6-3ca9-4695-b4c6-aea55b27c1e1'}
-        ];
-
-        const globalTBUrls = await Promise.all(
-          globalTBParams.map(async (params) => {
+        let params;
+        switch (activeTab) {
+          case 'globalTB':
+            params = globalTBParams;
+            break;
+          case 'countrySpecific':
+            params = countryParams;
+            break;
+          case 'demographics':
+            params = demographicsParams;
+            break;
+          case 'budgetExpenditure':
+            params = budgetExpenditureParams;
+            break;
+          default:
+            params = [];
+        }
+        
+        const urls = await Promise.all(
+          params.map(async (param) => {
             const response = await axios.get('https://v83joq8cn3.execute-api.us-east-1.amazonaws.com/visual/api/getUrl', {
-              params: params,
+              params: param,
             });
             return response.data.embedUrl;
           })
         );
 
-        const countryUrls = await Promise.all(
-          countryParams.map(async (params) => {
-            const response = await axios.get('https://v83joq8cn3.execute-api.us-east-1.amazonaws.com/visual/api/getUrl', {
-              params: params,
-            });
-            return response.data.embedUrl;
-          })
-        );
-
-        const demographicsUrls = await Promise.all(
-          demographicsParams.map(async (params) => {
-            const response = await axios.get('https://v83joq8cn3.execute-api.us-east-1.amazonaws.com/visual/api/getUrl', {
-              params: params,
-            });
-            return response.data.embedUrl;
-          })
-        );
-
-        const budgetExpenditureUrls = await Promise.all(
-          budgetExpenditureParams.map(async (params) => {
-            const response = await axios.get('https://v83joq8cn3.execute-api.us-east-1.amazonaws.com/visual/api/getUrl', {
-              params: params,
-            });
-            return response.data.embedUrl;
-          })
-        );
-
-        urlsData.quickSightGlobalTB = globalTBUrls;
-        urlsData.quickSightCountry =  countryUrls;
-        urlsData.quickSightDemographics = demographicsUrls;
-        urlsData.quickSightBudgetExpenditure = budgetExpenditureUrls;
-
-        setViewUrls(urlsData);
+        setViewUrls(prevUrls => ({ ...prevUrls, [activeTab]: urls }));
       } catch (error) {
         setErorr(error);
       }
     };
 
     fetchUrls();
-  });
+  }, [activeTab]);
 
   const renderMainLayout = (tab) => {
     switch (tab) {
